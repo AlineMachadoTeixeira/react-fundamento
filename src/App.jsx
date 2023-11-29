ad1548ba3148dd7f0ff54aee5bd9e9520f66581c
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-11-29 11:17:57 -0300

    componente único  html no Appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,33 @@ function App() {
              É normal ficar entre {} nessa situação */}
 
       <main>
-        <h2>Teste</h2>
+        <section>
+          <h2>Conteúdo da aolicação</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            totam ipsam unde nobis eaque fugiat qui molestiae dolores. Quas, ad!
+          </p>
+
+          <article>
+            <h3>Artigo 1</h3>
+            <p>Conteúdo do artigo 1.....</p>
+          </article>
+
+          <article>
+            <h3>Artigo 2</h3>
+            <p>Conteúdo do artigo 2.....</p>
+          </article>
+
+          <article>
+            <h3>Artigo 3</h3>
+            <p>Conteúdo do artigo 3.....</p>
+          </article>
+        </section>
       </main>
+
+      <footer>
+        <h2>Rodapé da aplicação</h2>
+      </footer>
     </>
   );
 }
