04481864063b450cb631b37fb4a532ab82aac8fa
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-11-30 09:03:55 -0300

    criando componente Menu

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,13 +1,12 @@
+import Menu from "./Menu";
+
 function Cabecalho() {
   return (
     <header>
       <h1>Olá Mundo, React!😉</h1>
       <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Produtos</a>
-        <a href="">Contato</a>
-      </nav>
+
+      <Menu />
     </header>
   );
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+function Menu() {
+  return (
+    <nav>
+      <a href="">Home</a>
+      <a href="">Produtos</a>
+      <a href="">Contato</a>
+    </nav>
+  );
+}
+
+export default Menu;
+//Exporte ele para Cabecalho.jsx
