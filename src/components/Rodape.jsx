624c33a64a40d4583ae5414ba185e500902d1be1
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-11-30 09:39:01 -0300

    No footer CSS inline com const

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,6 +1,12 @@
 function Rodape() {
+  const exemploEstilos = {
+    backgroundColor: "lightblue",
+    padding: "2%",
+    color: "blue",
+    border: "solid 4px #f00",
+  };
   return (
-    <footer style={{ backgroundColor: "lightblue", padding: "2%" }}>
+    <footer style={exemploEstilos}>
       {/* CSS inline */}
       <h2>Rodapé da aplicação</h2>
     </footer>
