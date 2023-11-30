3eb6c33a8d1b657441c94ece7a0059e41055918d
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-11-30 09:19:26 -0300

    Componente Artigo

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,3 +1,5 @@
+import Artigo from "./Artigo";
+
 function Conteudo() {
   return (
     <main>
@@ -8,20 +10,10 @@ function Conteudo() {
           totam ipsam unde nobis eaque fugiat qui molestiae dolores. Quas, ad!
         </p>
 
-        <article>
-          <h3>Artigo 1</h3>
-          <p>Conteúdo do artigo 1.....</p>
-        </article>
-
-        <article>
-          <h3>Artigo 2</h3>
-          <p>Conteúdo do artigo 2.....</p>
-        </article>
-
-        <article>
-          <h3>Artigo 3</h3>
-          <p>Conteúdo do artigo 3.....</p>
-        </article>
+        {/* Reaproveitamento de Componentes */}
+        <Artigo />
+        <Artigo />
+        <Artigo />
       </section>
     </main>
   );
