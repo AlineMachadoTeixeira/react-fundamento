b5ea15e58171d4e08c5fd3e828648e31af32bd7f
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-11-30 11:19:56 -0300

    class como fazer funcionar com module

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -8,7 +8,7 @@ function Cabecalho() {
   return (
     <header>
       <h1>
-        <span className="destaque-texto"> Olá </span> Mundo,{" "}
+        <span className={estilos["destaque-texto"]}> Olá </span> Mundo,
         <span className={estilos.sombra}>React!</span>😉
       </h1>
       <hr />
