5ec7155758a8d2794dce0615d38af1eed7a239b3
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-12-01 10:45:44 -0300

    StyledArtigo

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,9 +1,30 @@
+import styled from "styled-components";
+
+const StyledArtigo = styled.article`
+  background-color: #b6b6b69b;
+  padding: 1rem;
+  margin: 0.5rem 0;
+  /* display: flex;
+  flex-direction: column; */
+  text-align: center;
+
+  h3 {
+    border-bottom: 1px solid #b6b6b6;
+    //width: 20%;
+  }
+
+  p {
+    font-size: 1.1rem;
+  }
+`;
+
 function Artigo() {
   return (
-    <article>
+    // era article ficou StyledArtigo
+    <StyledArtigo>
       <h3>Artigo 1</h3>
       <p>Conteúdo do artigo 1.....</p>
-    </article>
+    </StyledArtigo>
   );
 }
 
